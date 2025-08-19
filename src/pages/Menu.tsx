b1d0e889cf6e659815f713d1d4d38779d342726d
Rafael Hernandez Pompeyo <antonio.hepo@gmail.com>
2025-08-18 18:33:07 -0600

    Changing only text of Main.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export default function Menu() {
   return (
     <div style={{ textAlign: "center", padding: "30px" }}>
-      <h2>Menú Principal</h2>
+      <h2>Formatos Salubridad</h2>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px", maxWidth: "300px", margin: "0 auto" }}>
         
         <Link to="/registro-de-ordenes-de-produccion">
-          <button style={{ padding: "10px", fontSize: "1rem", backgroundColor: "#56ccf2", color: "white", border: "none", borderRadius: "5px" }}>📄 Formatos Salubridad</button>
+          <button style={{ padding: "10px", fontSize: "1rem", backgroundColor: "#56ccf2", color: "white", border: "none", borderRadius: "5px" }}>📄 Registro de Ordenes de Producción</button>
         </Link>
 
 
